f17a0363800acef7180bd826509ffc48123abd73
Vizeet Srivastava <vizeet@gmail.com>
2023-02-24 00:29:50 +0100

    duplicate resource

--- Terraform/scripts/ecr_repos.tf
+++ Terraform/scripts/ecr_repos.tf
@@ -4,17 +4,17 @@ resource "aws_ecr_repository" "pdf_splitter" {
 }
 
 # Add tags to the Docker image
-resource "aws_ecr_image" "pdf_splitter" {
-  repository_name = aws_ecr_repository.pdf_splitter.name
-  image_tag = "latest"
-}
+#resource "aws_ecr_image" "pdf_splitter" {
+#  repository_name = aws_ecr_repository.pdf_splitter.name
+#  image_tag = "latest"
+#}
 
 resource "aws_ecr_repository" "page_extractor" {
   name = "page_extractor"
 }
 
 # Add tags to the Docker image
-resource "aws_ecr_image" "page_extractor" {
-  repository_name = aws_ecr_repository.page_extractor.name
-  image_tag = "latest"
-}
+#resource "aws_ecr_image" "page_extractor" {
+#  repository_name = aws_ecr_repository.page_extractor.name
+#  image_tag = "latest"
+#}
