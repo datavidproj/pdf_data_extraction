e4af104b4336c52b51224d52bcb5c95e36b7ed36
Vizeet Srivastava <vizeet@gmail.com>
2023-02-24 16:05:33 +0100

    duplicate resource

--- Terraform/scripts/sqs_queue.tf
+++ Terraform/scripts/sqs_queue.tf
@@ -6,5 +6,20 @@ resource "aws_lambda_event_source_mapping" "page_extractor_sqs_mapping" {
   event_source_arn  = aws_sqs_queue.pdf_page_info.arn
   function_name     = aws_lambda_function.page_extractor.arn
   batch_size        = 1
-#  starting_position = "TRIM_HORIZON"
+  enabled           = true
+  starting_position = "TRIM_HORIZON"
+}
+
+data "aws_iam_policy_document" "sqs_queue_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.pdf_page_info_queue.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_lambda_function.pdf_splitter_lambda_function.arn]
+    }
+  }
 }
