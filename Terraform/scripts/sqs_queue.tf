25663e3ec6a1a8e3e8e488d4d0ee34df8ee7cafd
Vizeet Srivastava <vizeet@gmail.com>
2023-02-24 14:41:33 +0100

    duplicate resource

--- Terraform/scripts/sqs_queue.tf
+++ Terraform/scripts/sqs_queue.tf
@@ -6,5 +6,5 @@ resource "aws_lambda_event_source_mapping" "page_extractor_sqs_mapping" {
   event_source_arn  = aws_sqs_queue.pdf_page_info.arn
   function_name     = aws_lambda_function.page_extractor.arn
   batch_size        = 1
-  starting_position = "TRIM_HORIZON"
+#  starting_position = "TRIM_HORIZON"
 }
