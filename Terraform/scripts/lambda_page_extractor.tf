0aa945d3fd2793554e67dcef77f06148437fb138
Vizeet Srivastava <vizeet@gmail.com>
2023-02-24 01:25:47 +0100

    duplicate resource

--- Terraform/scripts/lambda_page_extractor.tf
+++ Terraform/scripts/lambda_page_extractor.tf
@@ -3,7 +3,7 @@ data "aws_ecr_image" "page_extractor" {
   image_tag       = "latest"
 }
 
-data "aws_sqs_queue" "datavid_pdfconverter" {
+data "aws_sqs_queue" "pdf_page_info" {
   name = var.sqs_queue_name
 }
 
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "page_extractor" {
       TEMP_KEY_PREFIX=var.bbox_images_key_prefix
       MASKED_KEY_PREFIX=var.masked_images_key_prefix
       TABLE_CORNERS_KEY_PREFIX=var.table_corners_key_prefix
-      SQS_QUEUE_URL=data.aws_sqs_queue.datavid_pdfconverter.url
+      SQS_QUEUE_URL=data.aws_sqs_queue.pdf_page_info.url
     }
   }
 }
