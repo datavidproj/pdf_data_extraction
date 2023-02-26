9bb0d129af0f69eb76f807a1769fa5d1f3846e78
Vizeet Srivastava <vizeet@gmail.com>
2023-02-26 22:06:10 +0100

    env changes

--- Terraform/scripts/sqs_queue.tf
+++ Terraform/scripts/sqs_queue.tf
@@ -7,6 +7,7 @@ resource "aws_lambda_event_source_mapping" "page_extractor_sqs_mapping" {
   event_source_arn = aws_sqs_queue.pdf_page_info.arn
   function_name    = aws_lambda_function.page_extractor.arn
   batch_size       = var.batch_size
+  depends_on       = [aws_iam_role_policy_attachment.lambda_sqs_attachment]
 }
 
 #data "aws_iam_policy_document" "sqs_queue_policy" {
