e3cc34ec7c3c84c0e9898d9b6104662e47bd4182
vizeet srivastava <vizeet@gmail.com>
2023-02-27 22:43:12 +0000

    changes

--- Terraform/main/lambda_page_extractor.tf
+++ Terraform/main/lambda_page_extractor.tf
@@ -94,7 +94,8 @@ resource "aws_route_table_association" "private" {
 resource "aws_route" "s3" {
   route_table_id            = aws_route_table.private.id
 #  destination_cidr_block    = "com.amazonaws.${var.AWS_REGION}.s3"
-  destination_cidr_block    = "vpce.amazonaws.com"
+#  destination_cidr_block    = "vpce.amazonaws.com"
+  destination_cidr_block    = aws_vpc_endpoint.s3.cidr_blocks[0]
   vpc_endpoint_id           = aws_vpc_endpoint.s3.id
 }
 
